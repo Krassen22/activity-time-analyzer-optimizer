21a4da26059578238156c44f6b03faf9e0196346
Krassen22 <krassen22@gmail.com>
2016-03-15 23:19:25 +0200

    #2 Sign in page created

---
+++  WebApplication/WebContent/scripts/pSignIn.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	"use strict";
+	var ENDPOINT = "http://localhost:3000/RegularUser/";
+	function userEndpoint(userId) {
+		return ENDPOINT + "/" + userId;
+	}
+	
+	function createUser(user) {
+		return $.ajax(ENDPOINT, {
+			method: "POST",
+			dataType: "json",
+			data: JSON.stringify(user),
+			contentType: "application/json; charset=utf-8"
+		});
+	}
+	
+	function signInChecker(email, username, userObject) {
+		var	existUser = false;	
+		$.ajax(ENDPOINT, {
+			method: "GET",
+			dataType: "json"
+		}).then(function(response) {
+			_.forEach(response, function(user) {
+				if(user.email == email) {
+					alert("E-mail already in use");
+					existUser = true; 
+				}
+				if(user.username == username) {
+					alert("Username already in use");
+					existUser = true;
+				}
+			});
+			if(existUser != false) {
+				$("[name='email']").val("");
+				$("[name='password']").val("");
+				$("[name='username']").val("");
+			}else{
+				createUser(userObject);
+				window.location.assign("../pages/wallOf.html");
+			}
+		});
+	}
+	
+	function signIn(){
+		$("#createUserButton").click(function() {
+			var user = {
+				email: $("[name='email']").val(),
+				password: $("[name='password']").val(),
+				username: $("[name='username']").val()
+			};
+			signInChecker(user.email, user.username, user);
+		});
+	}
+	
+	signIn();
+});
